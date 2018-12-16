063d0b76a21afdc4810f4908f2c3aa7913fd7a92
huxiaowei <525782303@qq.com>
2018-12-16 21:32:41 +0800

    泛型---混型

---
+++ src/generics/mix/TimeStampedImp.java
@@ -0,0 +1,34 @@
+/**
+ * Project: MyTest
+ * <p>
+ * File Created at 2018/12/16
+ * <p>
+ * Copyright 2018 e-dewin.com Corporation Limited.
+ * All rights reserved.
+ * <p>
+ * This software is the confidential and proprietary information of
+ * dewin Company. ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with e-dewin.com.
+ */
+package generics.mix;
+
+import java.util.Date;
+
+/**
+ * @Description:
+ * @author: hxw
+ * @version 1.0
+ * @date: 2018/12/16 21:29
+ */
+class TimeStampedImp implements TimeStamped{
+    private final long timeStamp;
+    public TimeStampedImp() {
+        timeStamp = new Date().getTime();
+    }
+
+    public long getStamp() {
+        return timeStamp;
+    }
+}
