24a825438e7322905e0d6b6a344acb27c86ead1c
hxw <525782303@qq.com>
2019-01-03 09:06:52 +0800

    Thinking In Java --第16章 数组

--- src/arrays/ArrayOfGenerics.java
+++ src/arrays/ArrayOfGenerics.java
@@ -11,7 +11,7 @@ import java.util.List;
  */
 public class ArrayOfGenerics {
 
-    @SuppressWarnings("unchecked")
+    //@SuppressWarnings("unchecked")
     public static void main(String[] args) {
         List<String>[] ls;
         List[] la = new List[10];
