ecd13f157922a157d7f2ab4ad95c1dd842911991
hxw <525782303@qq.com>
2019-01-21 00:10:36 +0800

    Thinking In Java --第17章 容器深入

--- src/net/mindview/util/Countries.java
+++ src/net/mindview/util/Countries.java
@@ -131,15 +131,17 @@ public class Countries {
             {"URUGUAY","Montevideo"}, {"VENEZUELA","Caracas"},
     };
     // Use AbstractMap by implementing entrySet()
-    private static class FlyweightMap
-            extends AbstractMap<String,String> {
-        private static class Entry
-                implements Map.Entry<String,String> {
+    private static class FlyweightMap extends AbstractMap<String,String> {
+
+        /**
+         * FlyweightMap的静态内部类
+         */
+        private static class Entry implements Map.Entry<String,String> {
             int index;
             Entry(int index) { this.index = index; }
-            public boolean equals(Object o) {
-                return DATA[index][0].equals(o);
-            }
+
+            public boolean equals(Object o) { return DATA[index][0].equals(o); }
+
             public String getKey() { return DATA[index][0]; }
             public String getValue() { return DATA[index][1]; }
             public String setValue(String value) {
@@ -149,9 +151,12 @@ public class Countries {
                 return DATA[index][0].hashCode();
             }
         }
+
+        /**
+         * FlyweightMap的静态内部类
+         */
         // Use AbstractSet by implementing size() & iterator()
-        static class EntrySet
-                extends AbstractSet<Map.Entry<String,String>> {
+        static class EntrySet extends AbstractSet<Map.Entry<String,String>> {
             private int size;
             EntrySet(int size) {
                 if(size < 0)
@@ -162,9 +167,15 @@ public class Countries {
                 else
                     this.size = size;
             }
-            public int size() { return size; }
-            private class Iter
-                    implements Iterator<Map.Entry<String,String>> {
+
+            public int size() {
+                return size;
+            }
+
+            /**
+             * EntrySet的内部类
+             */
+            private class Iter implements Iterator<Map.Entry<String,String>> {
                 // Only one Entry object per Iterator:
                 private Entry entry = new Entry(-1);
                 public boolean hasNext() {
@@ -174,21 +185,25 @@ public class Countries {
                     entry.index++;
                     return entry;
                 }
+
                 public void remove() {
                     throw new UnsupportedOperationException();
                 }
             }
-            public
-            Iterator<Map.Entry<String,String>> iterator() {
+
+            public Iterator<Map.Entry<String,String>> iterator() {
                 return new Iter();
             }
         }
-        private static Set<Map.Entry<String,String>> entries =
-                new EntrySet(DATA.length);
+
+        private static Set<Map.Entry<String,String>> entries = new EntrySet(DATA.length);
+
         public Set<Map.Entry<String,String>> entrySet() {
             return entries;
         }
+
     }
+
     // Create a partial map of 'size' countries:
     static Map<String,String> select(final int size) {
         return new FlyweightMap() {
@@ -197,21 +212,27 @@ public class Countries {
             }
         };
     }
+
     static Map<String,String> map = new FlyweightMap();
+
     public static Map<String,String> capitals() {
         return map; // The entire map
     }
     public static Map<String,String> capitals(int size) {
         return select(size); // A partial map
     }
-    static List<String> names =
-            new ArrayList<String>(map.keySet());
+    static List<String> names = new ArrayList<String>(map.keySet());
+
     // All the names:
-    public static List<String> names() { return names; }
+    public static List<String> names() {
+        return names;
+    }
+
     // A partial list:
     public static List<String> names(int size) {
         return new ArrayList<String>(select(size).keySet());
     }
+
     public static void main(String[] args) {
         print(capitals(10));
         print(names(10));
