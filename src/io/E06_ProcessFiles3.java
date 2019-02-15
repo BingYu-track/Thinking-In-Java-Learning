abcfd3f80b01e2228f9b51ff199cf8d57bae5596
huxiaowei <525782303@qq.com>
2019-02-15 10:50:10 +0800

    Thinking In Java --第18章 IO

--- src/io/E06_ProcessFiles3.java
+++ src/io/E06_ProcessFiles3.java
@@ -9,7 +9,7 @@ import java.util.Locale;
 
 /**
  * @version 1.0
- * @Description: 使用ProcessFiles查找在特定日期之后修改的特定目录子树中的所有Java源代码文件。
+ * @Description: 使用ProcessFiles查找在传入的特定日期之后修改的特定目录子树中的所有Java源代码文件。
  * @author: hxw
  * @date: 2019/2/14 18:27
  */
@@ -23,20 +23,24 @@ public class E06_ProcessFiles3 {
         }
         long tmp = 0;
         try {
-            df.setLenient(false); //指定日期/时间 严格解析
-            tmp = df.parse(args[1]).getTime(); //DateFormat解析字符串生成日期
+            df.setLenient(false); //指定日期/时间进行严格解析
+            tmp = df.parse(args[1]).getTime(); //DateFormat解析字符串生成日期(该日期是毫秒数)
+            System.out.println("时间限制："+ tmp/1000); //表示时间：2006/1/1 0:0:0
         } catch(ParseException pe) {
             pe.printStackTrace();
             return;
         }
         final long modTime = tmp;
-        //重难点理解代码
-        new ProcessFiles(new ProcessFiles.Strategy() {
+        //初始化ProcessFiles并调用start()方法
+        ProcessFiles pf = new ProcessFiles(new ProcessFiles.Strategy() {
             public void process(File file) {
-                if(modTime < file.lastModified()){
-                    System.out.println(file);
+                long lastModified = file.lastModified();
+                if (modTime < lastModified) {
+                    System.out.println(file + " 最后修改时间:" + lastModified / 1000);
                 }
             }
-        }, "java").start(new String[] {args[0]});
+        }, "java");
+
+        pf.start(new String[] {args[0]});
     }
 }
