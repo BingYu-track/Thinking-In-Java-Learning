cd6386335d327d9ab74bcc6bd04fe1ceb2bd6ceb
huxiaowei <525782303@qq.com>
2021-06-22 21:48:53 +0800

    git忽略文件修改

--- src/enumerated/UpcastEnum.java
+++ src/enumerated/UpcastEnum.java
@@ -11,7 +11,7 @@ public class UpcastEnum {
 
     public static void main(String[] args) {
         Search[] vals = Search.values();
-        Enum e = Search.HITHER; // Upcast 向上转成Enum，此时无法再使用values()方法,但是可以使用e.getClass().getEnumConstants()获取所有的Enum实例
+        Enum e = Search.HITHER; // Upcast 将枚举实例向上转成Enum，此时无法再使用values()方法,但是可以使用e.getClass().getEnumConstants()获取所有的Enum实例
         // e.values(); // No values() in Enum
         for(Enum en : e.getClass().getEnumConstants()) {
             System.out.println(en);
